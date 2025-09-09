2741ef053d598d408f896a336722bc550cff85b3
SalustianoPosse-web <salustianoposse@gmail.com>
2025-09-09 16:16:33 -0300

    Delete Punto2 directory

--- Punto2/tareaDocumentacion.java
+++
@@ -1,8 +0,0 @@
-package Punto2;
-
-public class tareaDocumentacion extends tarea{
-   public tareaDocumentacion(int fechaInicio, int fechaFin, String responsable){
-       super(fechaInicio, fechaFin, responsable);
-   }
-
-}
